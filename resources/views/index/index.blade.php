850afe653c54183b4d569af602113d59672f86fc
Nikita <30903256+NikitosGolubev@users.noreply.github.com>
2019-03-07 23:18:32 +0300

    form basic styling

--- resources/views/index/index.blade.php
+++ resources/views/index/index.blade.php
@@ -10,18 +10,22 @@
 @endsection
 
 @section('main')
-    <div>
-        <div>
-            <div>
+    <div class='forms_wrap'>
+        <div class='forms'>
+            <div class='forms__header'>
                 <span>Choose the way to parse facts and load them to database:</span>
             </div>
             <div>
                 <form name='get_particular_num_facts_form' method='' action=''>
                     @csrf
-                    <div>
-                        <div><span>1</span></div>
-                        <div>
-                            <select>
+                    <div class='particular-num-facts-form parsing-form form-grid'>
+                    	<div class='form-grid__elem'>
+                            <div class='field field_default parsing-form__number_wrap'>
+                                <span class='parsing-form__number'>1</span>
+                            </div>
+                        </div>
+                        <div class='form-grid__elem'>
+                            <select class='field field_default'>
                                 <option value=''>Category</option>
                                 <option value='random'>Random</option>
                                 <option value='trivia'>Trivia</option>
@@ -30,11 +34,11 @@
                                 <option value='math'>Math</option>
                             </select>
                         </div>
-                        <div>
-                            <input type="text" name="num_facts" placeholder="Number of facts" />
+                        <div class='form-grid__elem'>
+                            <input class='field field_default' type="text" name="num_facts" placeholder="Number of facts" />
                         </div>
-                        <div>
-                            <input type='submit' name='get_num_facts' value='Parse' />
+                        <div class='form-grid__elem'>
+                            <input class='field' type='submit' name='get_num_facts' value='Parse' />
                         </div>
                     </div>
                 </form>
@@ -42,10 +46,14 @@
             <div>
                 <form name='get_facts_in_range_form' method='' action=''>
                     @csrf
-                    <div>
-                        <div><span>2</span></div>
-                        <div>
-                            <select>
+                    <div class='facts-in-range-form parsing-form form-grid'>
+                    	<div class='form-grid__elem'>
+                            <div class='field field_default parsing-form__number_wrap'>
+                                <span class='parsing-form__number'>2</span>
+                            </div>
+                        </div> 
+                        <div class='form-grid__elem'>
+                            <select class='field field_default'>
                                 <option value=''>Category</option>
                                 <option value='random'>Random</option>
                                 <option value='trivia'>Trivia</option>
@@ -54,12 +62,14 @@
                                 <option value='math'>Math</option>
                             </select>
                         </div>
-                        <div>
-                            <input type="text" name="from" placeholder="From:" />
-                            <input type="text" name="to" placeholder="To:" />
+                        <div class='form-grid__elem'>
+                            <input class='field field_default' type="text" name="from" placeholder="From:" />
                         </div>
-                        <div>
-                            <input type='submit' name='get_facts_in_range' value='Parse' />
+                        <div class='form-grid__elem'>
+                            <input class='field field_default' type="text" name="to" placeholder="To:" />
+                        </div>
+                        <div class='form-grid__elem'>
+                            <input class='field' type='submit' name='get_facts_in_range' value='Parse' />
                         </div>
                     </div>
                 </form>
@@ -67,61 +77,67 @@
             <div>
                 <form name='get_particular_facts_form' method='' action=''>
                     @csrf
-                    <div>
-                        <div><span>3</span></div>
-                        <div>
-                            <div>
-                                <div><span>Add something here...</span></div>
-                            </div>
-                            <div>
-                                <div><span>Particular fact numbers</span></div>
-                            </div>
-                            <div>
-                                <div>
-                                    <div><span>7832</span></div>
-                                    <button>X</button>
-                                </div>
-                                <div>
-                                    <div><span>34</span></div>
-                                    <button>X</button>
-                                </div>
-                                <div>
-                                    <div><span>5345</span></div>
-                                    <button>X</button>
-                                </div>
-                                <div>
-                                    <div><span>234</span></div>
-                                    <button>X</button>
-                                </div>
-                                <div>
-                                    <div><span>3242</span></div>
-                                    <button>X</button>
-                                </div>
-                                <div>
-                                    <div><span>654</span></div>
-                                    <button>X</button>
-                                </div>
-                                <div>
-                                    <div><span>21</span></div>
-                                    <button>X</button>
-                                </div>
-                                <div>
-                                    <div><span>7</span></div>
-                                    <button>X</button>
-                                </div>
+                    <div class='particular-facts-form parsing-form form-grid'>
+                    	<div class='form-grid__elem'>
+                            <div class='field field_default parsing-form__number_wrap'>
+                                <span class='parsing-form__number'>3</span>
                             </div>
                         </div>
-                        <div>
+                        <div class='form-grid__elem'>
+	                        <div class='field field_default'>
+	                            <div>
+	                                <div><span>Add something here...</span></div>
+	                            </div>
+	                            <div>
+	                                <div><span>Particular fact numbers</span></div>
+	                            </div>
+	                            <div>
+	                                <div>
+	                                    <div><span>7832</span></div>
+	                                    <button>X</button>
+	                                </div>
+	                                <div>
+	                                    <div><span>34</span></div>
+	                                    <button>X</button>
+	                                </div>
+	                                <div>
+	                                    <div><span>5345</span></div>
+	                                    <button>X</button>
+	                                </div>
+	                                <div>
+	                                    <div><span>234</span></div>
+	                                    <button>X</button>
+	                                </div>
+	                                <div>
+	                                    <div><span>3242</span></div>
+	                                    <button>X</button>
+	                                </div>
+	                                <div>
+	                                    <div><span>654</span></div>
+	                                    <button>X</button>
+	                                </div>
+	                                <div>
+	                                    <div><span>21</span></div>
+	                                    <button>X</button>
+	                                </div>
+	                                <div>
+	                                    <div><span>7</span></div>
+	                                    <button>X</button>
+	                                </div>
+	                            </div>
+	                        </div>
+	                    </div>
+                        <div class='form-grid__elem'>
                             <div>
                                 <div>
-                                    <input type="text" name="number_to_add" placeholder="Number:" />
+                                    <input class='field field_default' type="text" name="number_to_add" placeholder="Number:" />
                                 </div>
                                 <div>
-                                    <input type="button" name="add_number" value="Add" />
+                                    <input class='field' type="button" name="add_number" value="Add" />
                                 </div>
                             </div>
                             <div>
-                                <input type='submit' name='get_particular_facts' value='Parse' />
+                                <input class='field' type='submit' name='get_particular_facts' value='Parse' />
                             </div>
                         </div>
                     </div>
